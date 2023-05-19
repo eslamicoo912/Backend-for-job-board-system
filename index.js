a2fd0fa3da8473d954fcf9892fd328ac4aa02335
eslamicoo912 <eslamicoo3@gmail.com>
2023-05-19 12:25:27 +0300

    set up the basic npm packages and the index file

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// database connection
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening for port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
