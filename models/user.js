8b49fce01c02cd9f459b965216b261400eb332c3
eslamicoo912 <eslamicoo3@gmail.com>
2023-05-28 15:15:31 +0300

    creating the database model for the user

---
+++ models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const User = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("UserModel", user);
+
+export default UserModel;
